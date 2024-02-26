1811ed181de4cfb67ad5dfa3faa7d226c8463493
Emil <buraindo24@gmail.com>
2024-02-26 12:16:28 +0300

    slice

--- programs/usvm/slice.go
+++ programs/usvm/slice.go
@@ -76,8 +76,26 @@ func castSlice(arr []int) []int {
 }
 
 func sliceToArrayPointer(arr []int) int {
+	if arr[2] == arr[3] {
+		return -1
+	}
 	a := (*[4]int)(arr)
 	return a[2]
+}
+
+func sliceArray(arr []int) []int {
+	if arr[0] == arr[1] {
+		return arr[0:1]
+	}
+	return arr[1:3]
+}
+
+func sliceArrayArgs(arr []int, low, high int) []int {
+	return arr[low:high]
+}
+
+func sliceString(s string) string {
+	return s[1:3]
 }
 
 func main() {}
